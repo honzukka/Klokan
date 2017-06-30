023963f426ddc93b47778a1bde62f29edacdf233
honzukka <randomnonplayercharacter@gmail.com>
2017-06-30 21:54:32 +0200

    02-002-strings nad structs are working

--- klokan/test.cpp
+++ klokan/test.cpp
@@ -1,6 +1,78 @@
 #include <iostream>
+#include <string>
+#include <array>
+
+using namespace std;
+
+using testTableAnswers = array<array<bool, 2>, 3>;
+using testSheetAnswers = array<testTableAnswers, 2>;
+
+#pragma pack(push, 8)
+struct MyStructure
+{
+	int number1;
+	float number2;
+	int number3;
+	int number4;
+};
+#pragma pack(pop)
 
 extern "C" __declspec(dllexport) void __stdcall hello_world()
 {
-	std::cout << "Hello from C++!!!" << std::endl;
+	cout << "Hello from C++!!!" << endl;
+	cout << endl;
+}
+
+// conversion happens on the .NET side (during marshalling)
+extern "C" __declspec(dllexport) void __stdcall string_test(char* text)
+{
+	string textString(text);
+
+	cout << "This was sent to C++: " << textString << endl;
+	cout << endl;
+}
+
+// conversion happens right here
+extern "C" __declspec(dllexport) void __stdcall string_test_wide(wchar_t* text)
+{
+	wstring textWString(text);
+	string textString(textWString.begin(), textWString.end());
+
+	cout << "This was sent to C++: " << textString << endl;
+	cout << endl;
+}
+
+extern "C" __declspec(dllexport) void __stdcall structure_test(MyStructure structure)
+{
+	cout << "This was sent to C++:" << endl;
+	cout << "Number 1: " << structure.number1 << endl;
+	cout << "Number 2: " << structure.number2 << endl;
+	cout << "Number 3: " << structure.number3 << endl;
+	cout << "Number 4: " << structure.number4 << endl;
+	cout << endl;
+}
+
+// assumming I get a buffer of int[2*3*2]...
+extern "C" _declspec(dllexport) void __stdcall array_test(int* answers)
+{
+	const int rows = 3;
+	const int cols = 2;
+	
+	// first table
+	for (size_t row = 0; row < rows; row++)
+	{
+		for (size_t col = 0; col < 2; col++)
+		{
+			answers[0 * rows * cols + row * cols + cols] = row + col;
+		}
+	}
+
+	// second table
+	for (size_t row = 0; row < cols; row++)
+	{
+		for (size_t col = 0; col < 2; col++)
+		{
+			answers[1 * rows * cols + row * cols + cols] = row + col + 5;
+		}
+	}
 }
--- klokantest/Program.cs
+++ klokantest/Program.cs
@@ -12,6 +12,35 @@ namespace klokantest
 	{
 		[DllImport("klokan.dll")]
 		public static extern void hello_world();
+
+		[DllImport("klokan.dll")]
+		public static extern void string_test(string text);
+
+		[DllImport("klokan.dll", CharSet = CharSet.Unicode)]
+		public static extern void string_test_wide(string text);
+
+		[DllImport("klokan.dll")]
+		public static extern void structure_test(MyAmazingStructure structure);
+
+		[DllImport("klokan.dll")]
+		public static extern void array_test(ref AnswerWrapper answers);
+	}
+
+	[StructLayout(LayoutKind.Sequential, Pack = 8)]
+	struct MyAmazingStructure
+	{
+		public int number1;
+		public float numberFloat;
+		public int number3;
+		public int number4;
+	}
+
+	// unsafe!!!
+	// TODO: finish this!
+	[StructLayout(LayoutKind.Sequential, Pack = 8)]
+	internal unsafe struct AnswerWrapper
+	{
+		public fixed int answers[2 * 3 * 2];
 	}
 
 	class Program
@@ -19,6 +48,13 @@ namespace klokantest
 		static void Main(string[] args)
 		{
 			Native.hello_world();
+			Native.string_test("Tak to jsem teda zvedavej!");
+			Native.string_test_wide("Tak to jsem teda zvedavej!");
+
+			MyAmazingStructure structure = new MyAmazingStructure { number1 = 2, numberFloat = 567.98f, number3 = 2000000000, number4 = 0 };
+			Native.structure_test(structure);
+
+			// .. arrays missing!
 		}
 	}
 }
